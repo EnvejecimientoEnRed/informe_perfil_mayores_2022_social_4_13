f40fbdc9175eac58fb875b60b42c4acf47140bfd
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-28 15:39:38 +0200

    Márgenes

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -27,7 +27,7 @@ export function initChart() {
         ///// Desarrollo del gráfico
         let paths;
 
-        let margin = {top: 10, right: 10, bottom: 30, left: 35},
+        let margin = {top: 12.5, right: 10, bottom: 25, left: 35},
             width = document.getElementById('chart').clientWidth - margin.left - margin.right,
             height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
 
